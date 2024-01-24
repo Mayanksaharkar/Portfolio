7cd0b5ec39c9a6f02b2dffa5bfaab01b9ab00da7
Mayank Saharkar <mayanksaharkar@gmail.com>
2024-01-24 13:41:16 +0530

    Routing Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
-function App() {
-  
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Portfolio from "./Components/Portfolio/Portfolio";
+import About from "./Components/About/About";
+import Layout from "./Components/Layout";
 
-  return (
-    <>
-     
-    </>
-  )
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/home",
+          element: <Home />,
+        },
+        {
+          path: "/portfolio",
+          element: <Portfolio />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
