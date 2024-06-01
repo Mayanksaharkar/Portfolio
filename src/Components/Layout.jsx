26b40d06820f17c06cb0113bc0fc2cfaa9353a62
Mayank Saharkar <mayanksaharkar@gmail.com>
2024-06-01 21:53:58 +0530

    svg animation

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-import { Fade } from "react-reveal";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+// import { Fade } from "react-reveal";
 
 function Layout() {
   return (
     <>
       <div className='lg:px-80 md:px-4 sm:px-2'>
-        <Fade bottom>
-          <div className='sticky top-0 z-50 backdrop-blur-lg   rounded-3xl border border-base-300'>
-            <Header />
-          </div>
-          <Outlet />
-          <div className='drop-shadow-xl'>
-            <Footer />
-          </div>
-        </Fade>
+        {/* <Fade bottom> */}
+        <div className='sticky top-0 z-50 backdrop-blur-lg   rounded-3xl border border-base-300'>
+          <Header />
+        </div>
+        <Outlet />
+        <div className='drop-shadow-xl'>
+          <Footer />
+        </div>
+        {/* </Fade> */}
       </div>
     </>
   );
