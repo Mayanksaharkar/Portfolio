0fb086c1356da023a65900d5798b654a94854797
Mayanksaharkar <mayanksaharkar@gmail.com>
2025-01-05 23:58:11 +0530

    project card fix
    Signed-off-by: Mayanksaharkar <mayanksaharkar@gmail.com>

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -6,23 +6,28 @@ const Projects = [
     title: "Link Shortner",
     desc: " Your user-friendly companion featuring 15+ themes for effortless computation tailored to your needs",
     img: Link_Shortner,
+    gh: "https://github.com/Mayanksaharkar/Link_Shortner",
+    live: "https://mayanksaharkar.github.io/Link_Shortner/",
   },
 ];
 function Portfolio() {
   return (
     <PageContainer>
-      <div className='px-8 '>
-        <h3 className='lg:text-3xl md:text-md sm:text-xl font-extrabold text-neutral py-4'>
+      <div className="px-8 ">
+        <h3 className="lg:text-3xl md:text-md sm:text-xl font-extrabold text-neutral py-4">
           my portfolio
         </h3>
-        <div className='grid grid-rows-3 gap-4'></div>
         {Projects.map((project) => (
-          <Card
-            key={project.title}
-            title={project.title}
-            desc={project.desc}
-            img={project.img}
-          />
+          <div className="w-full h-full my-2" key={project.gh}>
+            <Card
+              key={project.title}
+              title={project.title}
+              desc={project.desc}
+              img={project.img}
+              gh={project.gh}
+              live={project.live}
+            />
+          </div>
         ))}
       </div>
     </PageContainer>
