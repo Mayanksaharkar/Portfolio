b791683aac086db65ae138dadc1353424de48c5b
Mayanksaharkar <mayanksaharkar@gmail.com>
2025-05-16 00:35:03 +0530

    add: resume button

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
   const nav = useNavigate();
   return (
     <PageContainer>
-      <section className="z-40 min-h-[90dvh] flex flex-col justify-center items-center bg-transparent">
+      <section className="z-40 min-h-[80dvh] mb-16 flex flex-col justify-center items-center bg-transparent">
         <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-4">
           <div className="px-4 lg:px-8 mt-4 lg:mt-0">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">
@@ -19,12 +19,37 @@ function Home() {
             <p className="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl">
               Code that works. Solutions that matter
             </p>
-            <button
-              className="btn bg-primary text-base-100 shadow-base-300 hover:text-primary shadow-lg border-none rounded-lg lg:text-2xl md:text-md sm:text-sm"
-              onClick={() => nav("/projects")}
-            >
-              Browse Projects
-            </button>
+            <div>
+              <button
+                className="btn bg-primary text-base-100 shadow-base-300 hover:text-primary shadow-lg border-none rounded-lg lg:text-2xl md:text-md sm:text-sm"
+                onClick={() => nav("/projects")}
+              >
+                Browse Projects
+              </button>
+              <a
+                className="btn btn-outline border-primary text-primary hover:bg-transparent hover:text-primary hover:border-primary hover:shadow-lg hover:shadow-primary shadow-base-300  shadow-lg rounded-lg lg:text-2xl md:text-md sm:text-sm ml-4"
+                href="https://drive.google.com/file/d/1TCR_oInFSfxtj5nMAHJv-JXiM3wWe1hS/view?usp=drive_link"
+                target="_blank"
+              >
+                Resume
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="lucide lucide-download-icon lucide-download"
+                >
+                  <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+                  <polyline points="7 10 12 15 17 10" />
+                  <line x1="12" x2="12" y1="15" y2="3" />
+                </svg>
+              </a>
+            </div>
           </div>
 
           <CardContainer className="hidden lg:flex justify-center">
@@ -46,7 +71,7 @@ function Home() {
           <Education />
         </div>
       </section>
-    </ PageContainer>
+    </PageContainer>
   );
 }
 
