0f7d80f6e7ca792a8358eb33bb156173e6c5a8c3
Mayanksaharkar <mayanksaharkar@gmail.com>
2025-05-24 01:06:16 +0530

    refactor : proj sequesce

--- src/Components/Projects/data.jsx
+++ src/Components/Projects/data.jsx
@@ -18,12 +18,6 @@ const projects = [
     description: "MERN | OpenAI | TailwindCSS",
   },
   {
-    title: "Link Shortner",
-    img: "https://res.cloudinary.com/defcox81s/image/upload/v1747044708/linkShortner.jpg",
-    gh: "https://github.com/Mayanksaharkar/Link_Shortner",
-    description: "ReactJS | TailwindCSS | DaisyUI",
-  },
-  {
     title: "Time Table Generator",
     img: "https://res.cloudinary.com/defcox81s/image/upload/v1747049928/timeTable.jpg",
     gh: "https://github.com/Mayanksaharkar/Time_Table_Generator",
@@ -36,17 +30,23 @@ const projects = [
     description: "Python | BeautifulSoup | Requests",
   },
   {
-    title: "Calculator App",
-    img: "https://res.cloudinary.com/defcox81s/image/upload/v1747046706/calculator.jpg",
-    gh: "https://github.com/Mayanksaharkar/Calculator",
-    description: "ReactJS | DaisyUI ",
-  },
-  
-  {
     title: "OCR-CLI Tool",
     img: "https://res.cloudinary.com/defcox81s/image/upload/v1747308536/ocr_jhuadi.png",
     gh: "https://github.com/Mayanksaharkar/ocr_cli",
     description: "Python | Tesseract",
+  },
+  {
+    title: "Link Shortner",
+    img: "https://res.cloudinary.com/defcox81s/image/upload/v1747044708/linkShortner.jpg",
+    gh: "https://github.com/Mayanksaharkar/Link_Shortner",
+    description: "ReactJS | TailwindCSS | DaisyUI",
+  },
+
+  {
+    title: "Calculator App",
+    img: "https://res.cloudinary.com/defcox81s/image/upload/v1747046706/calculator.jpg",
+    gh: "https://github.com/Mayanksaharkar/Calculator",
+    description: "ReactJS | DaisyUI ",
   },
 ];
 
