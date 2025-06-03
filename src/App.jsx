01a168335c87aefefa586f824205361b96381406
Mayanksaharkar <mayanksaharkar@gmail.com>
2025-06-03 13:24:43 +0530

    refactor : disable cursor bor sm md

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
@@ -11,6 +11,14 @@ import AnimatedCursor from "react-animated-cursor";
 
 function App() {
   const [showSplash, setShowSplash] = useState(true);
+  const [isDesktop, setIsDesktop] = useState(true);
+
+  useEffect(() => {
+    const checkScreen = () => setIsDesktop(window.innerWidth > 768);
+    checkScreen();
+    window.addEventListener("resize", checkScreen);
+    return () => window.removeEventListener("resize", checkScreen);
+  }, []);
 
   const router = createBrowserRouter([
     {
@@ -51,25 +59,27 @@ function App() {
 
   return (
     <>
-   <AnimatedCursor
-        innerSize={12}
-        outerSize={32}
-        color="101, 135, 255"
-        outerColor="101, 135, 255"
-        outerAlpha={0.3}
-        innerScale={1}
-        outerScale={2}
-        clickables={[
-          "a",
-          "button",
-          ".link",
-          ".link-2",
-          ".link-3",
-          ".link-4",
-          ".link-5",
-          ".link-6",
-        ]}
-      />
+      {isDesktop && (
+        <AnimatedCursor
+          innerSize={12}
+          outerSize={32}
+          color="101, 135, 255"
+          outerColor="101, 135, 255"
+          outerAlpha={0.3}
+          innerScale={1}
+          outerScale={2}
+          clickables={[
+            "a",
+            "button",
+            ".link",
+            ".link-2",
+            ".link-3",
+            ".link-4",
+            ".link-5",
+            ".link-6",
+          ]}
+        />
+      )}
       <RouterProvider router={router} />
     </>
   );
